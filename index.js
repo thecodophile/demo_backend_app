ebf88ac3d119f6b82291f2f084dae15ce395c1a6
Somnath Dey <sdey.aklakhi@gmail.com>
2024-03-31 17:59:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// // used to parse request.body in express
+// const bodyParser = require("body-parser")
+// // specifically parse Json data & add it to the request.body object
+// app.use(bodyParser.json())
+
+// Adding Middleware to parse Json
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>This is App's homepage</h1>");
+});
+
+app.post("/car", (req, res) => {
+  res.send("Recieved a post request");
+});
+
+app.listen(port, () => {
+  console.log(`App is started on port ${port}`);
+});
+
+// const mongoose = require("mongoose");
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/vehicle", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connection Successful");
+//   })
+//   .catch((error) => {
+//     console.log("Recieved an error");
+//   });
